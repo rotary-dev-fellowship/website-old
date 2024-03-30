f494a3dd372bd32418da652cced5d5195f19f8a3
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2024-03-30 01:18:02 -0500

    docs: add documentation and @todos for future review

--- src/pages/og/[...slug].ts
+++ src/pages/og/[...slug].ts
@@ -1,3 +1,10 @@
+/**
+ * This file is used to generate Open Graph images for each collection pages:
+ * - http://localhost:4321/blog/post-2        <-- collection page url
+ * - http://localhost:4321/og/blog/post-2.png <-- generated og image
+ *                         ^^^           ^^^^
+ */
+
 import { getCollection, type ContentEntryMap } from "astro:content";
 import { OGImageRoute } from "astro-og-canvas";
 import { collections } from "@/content/config";
@@ -12,8 +19,12 @@ for (const collectionName of _collections) {
 }
 
 const pages = Object.fromEntries(entries.map(({ collection, slug, data }) => {
-  // @todo This feels hacky. Why is the docs collection not using the same
-  // path as the other collections?
+  // @todo Why is the `docs` collection not using a base path as the other
+  // collections? This might cause collisions with pages published in the
+  // `pages` directory.
+  // http://localhost:4321/guides/getting-started      (actual)
+  // http://localhost:4321/docs/guides/getting-started (suggested)
+  //                       ^^^^
   const staticPath = collection === 'docs'
     ? slug
     : `${collection}/${slug}`;
@@ -26,6 +37,8 @@ export const { getStaticPaths, GET } = OGImageRoute({
   param: "slug", // The parameter name in the URL.
   // This function is called for each page to get the options for the image.
   getImageOptions: (_path, page: (typeof pages)[number]) => {
+    // @todo This is a bit hacky. Should we enforce that the title and
+    // description are always required in the collection schemas?
     const title = (page.title === undefined && typeof page.title === 'string')
       ? page.title
       : SITE.title;
