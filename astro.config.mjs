f62542005a1e01fe067ca08aa70094def013174e
Ricardo Tribaldos <579671+tribal2@users.noreply.github.com>
2024-04-02 23:30:49 -0500

    Move Starlight configuration into its own file
    This works, but it won't auto update when you make changes to docs.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
+import { starlightConfig } from "./docs.config.mjs";
 
 // https://astro.build/config
 export default defineConfig({
@@ -26,72 +27,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     sitemap(),
-    starlight({
-      title: "ScrewFast Docs",
-      defaultLocale: "root",
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-        de: { label: "Deutsch", lang: "de" },
-        es: { label: "Español", lang: "es" },
-        fa: { label: "Persian", lang: "fa", dir: "rtl" },
-        fr: { label: "Français", lang: "fr" },
-        ja: { label: "日本語", lang: "ja" },
-        "zh-cn": { label: "简体中文", lang: "zh-CN" },
-      },
-      // https://starlight.astro.build/guides/sidebar/
-      sidebar: [
-        {
-          label: "Quick Start Guides",
-          translations: {
-            de: "Schnellstartanleitungen",
-            es: "Guías de Inicio Rápido",
-            fa: "راهنمای شروع سریع",
-            fr: "Guides de Démarrage Rapide",
-            ja: "クイックスタートガイド",
-            "zh-cn": "快速入门指南",
-          },
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Tools & Equipment",
-          items: [
-            { label: "Tool Guides", link: "tools/tool-guides/" },
-            { label: "Equipment Care", link: "tools/equipment-care/" },
-          ],
-        },
-        {
-          label: "Construction Services",
-          autogenerate: { directory: "construction" },
-        },
-        {
-          label: "Advanced Topics",
-          autogenerate: { directory: "advanced" },
-        },
-      ],
-      social: {
-        github: "https://github.com/mearashadowfax/ScrewFast",
-      },
-      disable404Route: true,
-      customCss: ["./src/styles/starlight.css"],
-      favicon: "/favicon.ico",
-      components: {
-        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-        Head: "./src/components/ui/starlight/Head.astro",
-      },
-      head: [
-        {
-          tag: "meta",
-          attrs: { property: "og:image", content: "https://screwfast.uk" + "/social.webp" },
-        },
-        {
-          tag: "meta",
-          attrs: { property: "twitter:image", content: "https://screwfast.uk" + "/social.webp" },
-        },
-      ],
-    }),
+    starlight(starlightConfig),
     compressor({
       gzip: false,
       brotli: true,
